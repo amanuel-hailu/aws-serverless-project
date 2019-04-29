e0445d70e6f62cd6e55c6a4c26c6095ad86e14c2
amanuel-hailu <amanuel.hailu@email.sitel.org>
2019-04-29 12:17:56 -0400

    edits

--- src/HelloLambda.py
+++ src/HelloLambda.py
@@ -2,6 +2,6 @@ def lambda_handler(event, context):
     print(event)
     response = {
         "statusCode": 200,
-        "body": "Hello I hope this works!!"
+        "body": "Hello, this is HelloLambda!!!"
     }
     return response
